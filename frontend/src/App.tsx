4dda012f1100140faee994f5a7405338c5329518
Ojasv Agrawal <ojasvagrawal2807@gmail.com>
2025-10-15 18:28:46 +0530

    new update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route, NavLink, useLocation } from "react-router-dom"
+import { Routes, Route, NavLink } from "react-router-dom"
 import Dashboard from "./pages/Dashboard"
 import Cowrie from "./pages/Cowrie"
 import AnalyticsDashboard from "./pages/AnalyticsDashboard"
